ca836cc40ecf2c3dc5d49031496128293f24d26d
kris12205 <mini429h@gmail.com>
2026-03-05 09:39:44 +0100

    Added ingredients

--- src/Ingredients.java
+++ src/Ingredients.java
@@ -1,10 +1,13 @@
 public abstract class Ingredients {
 
-    private String Name;
+    private String name;
     private double amount;
     private Units units;
 
-    public Ingredients(String name, double amount, Units units,){
+    public Ingredients(String name, double amount, Units units){
+        this.name = name;
+        this.amount = amount;
+        this.units = units;
 
     }
 }
